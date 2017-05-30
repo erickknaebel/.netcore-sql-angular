98f9b770ad61a3e55664e4debab369e37780601c
Erick Knaebel <erick.knaebel@gmail.com>
2017-05-30 10:58:50 -0400

    Add data annotations to Model and Make.

--- Models/Model.cs
+++ Models/Model.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DealerApp.Models
 {
     public class Model
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public Make Make { get; set; }
