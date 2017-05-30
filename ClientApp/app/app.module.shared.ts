90d8169d3668ce5a05740fdc76209c70b0be28a1
Erick Knaebel <erick.knaebel@gmail.com>
2017-05-30 15:47:48 -0400

    Add Angular component 'vehicle-form' and update router component to display the vehicle form.

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 
+import { VehicleFormComponent} from './components/vehicle-form/vehicle-form.component'
+
 export const sharedConfig: NgModule = {
     bootstrap: [ AppComponent ],
     declarations: [
@@ -14,11 +16,13 @@ export const sharedConfig: NgModule = {
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        VehicleFormComponent
     ],
     imports: [
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'vehicles/new', component: VehicleFormComponent },
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
