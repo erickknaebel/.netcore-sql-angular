1e8dc59eff22e3be4fdbcbfe3f95bcd64abd994a
Erick Knaebel <erick.knaebel@gmail.com>
2017-05-30 15:15:03 -0400

    Use AutoMapper to map domain models to domain rescources.

--- Controllers/MakesController.cs
+++ Controllers/MakesController.cs
@@ -4,22 +4,28 @@ using DealerApp.Models;
 using DealerApp.Persistence;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace dotnetcore_sql_angular.Controllers
 {
     public class MakesController : Controller
     {
         private readonly DealerDbContext context;
+        
+        public IMapper Mapper { get; }
 
-        public MakesController(DealerDbContext context)
+        public MakesController(DealerDbContext context, IMapper mapper)
         {
+            this.Mapper = mapper;
             this.context = context;
         }
 
         [HttpGet("/api/makes")]
-        public async Task<IEnumerable<Make>> GetMakes() 
+        public async Task<IEnumerable<Resources.MakeResource>> GetMakes()
         {
-            return await context.Makes.Include(m => m.Models).ToListAsync();
+            var makes = await context.Makes.Include(m => m.Models).ToListAsync();
+
+            return Mapper.Map<List<Make>, List<Resources.MakeResource>>(makes);
         }
     }
 }
---
+++ Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using DealerApp.Models;
+using dotnetcore_sql_angular.Resources;
+
+namespace dotnetcore_sql_angular.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Make, MakeResource>();
+            CreateMap<Model, ModelResource>();
+        }
+    }
+}
